502d30a5c085465198ed0c1bf6818b12d938ac2b
Indranil Mukherjee <bhaikaju@gmail.com>
2020-03-29 23:07:30 +0530

    Finishing Cart Component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartModelServer} from '../../models/cart.model';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: number;
+  subTotal: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public cartService: CartService) {
   }
 
+  ngOnInit(): void {
+    this.cartService.cartData$.subscribe((data: CartModelServer) => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+  }
+
+
+  ChangeQuantity(index: number, increase: boolean) {
+    this.cartService.UpdateCartItems(index, increase);
+  }
 }
